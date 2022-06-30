a637dfcb4ecab842cb1683827205d92c85b02584
fireblaze94 <fireblaze.ij@gmail.com>
2022-06-30 11:48:10 -0700

    first c code

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,23 +1,11 @@
-#include <stdio.h>                                                                                                                                       
-
-/**                                                                                                                                                      
- *
- *  * main - start of program                                                                                                                               
- *
- *   * print a statement using printf                                                                                                           
- *
- *    * Return: 0 as requested                                                                                                                                
- *
- *     */                                                                                                                                                      
-
-int main(void)                                                                                                                                           
-
-{                                                                                                                                                        
-
-	        printf("with proper grammar, but the outcome is a piece of art,\n");                                                                                    
-
-		        return (0);                                                                                                                                      
-
-			                                                                                                                                                         
-
-}      
+#include <stdio.h>
+/**
+ * main - start of program
+ * print a statement using printf
+ * Return: 0 as requested
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
