5b8941ebb0e171ca8f766f52020c52887a4cf7fc
fireblaze94 <fireblaze.ij@gmail.com>
2022-06-30 13:28:26 -0700

    first c code

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - program start
+ * Return: Always 1 as requested
+ */
+int main(void)
+{
+		write(2, "and that piece of art is useful\n"
+				" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
+}
