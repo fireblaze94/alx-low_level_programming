4d26d9d547e7236029cde9d118bf4b54d1a883db
fireblaze94 <fireblaze.ij@gmail.com>
2022-06-30 11:33:47 -0700

    first c code

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>                                                                                                                                       
+
+/**                                                                                                                                                      
+ *
+ *  * main - start of program                                                                                                                               
+ *
+ *   * print a statement using printf                                                                                                           
+ *
+ *    * Return: 0 as requested                                                                                                                                
+ *
+ *     */                                                                                                                                                      
+
+int main(void)                                                                                                                                           
+
+{                                                                                                                                                        
+
+	        printf("with proper grammar, but the outcome is a piece of art,");                                                                                    
+
+		        return (0);                                                                                                                                      
+
+			                                                                                                                                                         
+
+}      
