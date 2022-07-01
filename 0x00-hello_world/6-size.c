b3d492d19cf541837254ee9879d03649e0323a78
fireblaze94 <fireblaze.ij@gmail.com>
2022-06-30 17:39:40 -0700

    new2

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+
 /**
  * main - size of various types
  * print the sizes in bytes of various types
  * Return: 0 as requuested
  */
-int main(void)
 
+int main(void)
 {
 	int a;
 	long int b;
@@ -19,5 +20,4 @@ int main(void)
 	printf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(c);
 	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
 	return (0);
-
 }
