5a017b853b95adb26483503ee2851f25fa2ffaa7
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-02 08:24:05 -0700

    m

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,15 +10,18 @@ int main(void)
 {
 	int i, j;
 
-	for (i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '98'; i++)
 	{
-		for (j = i + 1; j <= '9'; j++)
+		for (j = i + 1; j <= '99'; j++)
 		{
-			putchar(i);
-			putchar(j);
+			putchar((p / 10) + '0');
+			putchar((p % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
 		}
 
-		if ( i != '8' || j != '9')
+		if ( i != '98' || j != '99')
 		{
 			putchar(',');
 			putchar(' ');
