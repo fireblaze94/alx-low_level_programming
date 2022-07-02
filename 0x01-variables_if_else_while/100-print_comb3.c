931545ebb637497e23987afcba20b12cf019dd22
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-02 07:44:01 -0700

    m

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start program
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = '0';  <= '9'; i++)
+	{
+		for (j = i+1; j <= '9'; j++)
+		{
+			if (j > i)
+			{
+				putchar(i);
+				putchar(j);
+			}
+
+			if (i != '8' && j != '9')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
