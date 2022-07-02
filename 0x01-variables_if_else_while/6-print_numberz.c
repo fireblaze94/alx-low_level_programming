3281bab79c9a09b9f3a29f3e5fb264016b03acb1
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-02 06:59:16 -0700

    m

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start program
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+	return 0;
+}
