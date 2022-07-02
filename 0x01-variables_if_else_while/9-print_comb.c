c6c9ffe6643d4c2e3a7e9f56af7f64c47d65fcd6
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-02 07:25:59 -0700

    m

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdo.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start program
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+
+		if (i != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
