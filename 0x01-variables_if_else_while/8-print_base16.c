75870cac9baa299f3b993e4dca47abc1b56fe395
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-02 07:12:33 -0700

    m

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start program
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char ch;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+
+	for (ch = 'a'; ch <='f'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
