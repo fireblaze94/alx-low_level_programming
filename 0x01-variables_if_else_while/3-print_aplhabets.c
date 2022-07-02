354b177603dcda25607b2d341bc398b0985b0b40
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-02 06:36:56 -0700

    m

---
+++ 0x01-variables_if_else_while/3-print_aplhabets.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	for (ch ='A'; ch <= 'Z'; ch++)
+	{
+		putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
