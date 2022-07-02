f1b3b8693175169d6caba2dbe6f437c83ce4cb50
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-02 06:44:57 -0700

    m

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start point
+ *
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char ch, e, q;
+	
+	e = 'e';
+	q = 'q';
+
+	for  (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (ch != e && ch != q)
+			putchar(ch);
+	}
+
+	putchar('\n');
+	return (0);
+}
