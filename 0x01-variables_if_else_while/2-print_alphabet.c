0d9e1bcd8f8951eb6c481b7ab8aff3baa34a6d21
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-02 06:17:56 -0700

    m

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - start point
+ * print alphabets with space
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char smallChar;
+	for (smallChar = 'a'; smallChar <= 'z'; smallChar++)
+		putchar(smallChar);
+		putchar('\n');
+
+	return (0);
+}
