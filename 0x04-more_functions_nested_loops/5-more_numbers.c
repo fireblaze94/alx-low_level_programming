55e706a9bfa24613299d4fbde28887b5925ba005
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-08 07:38:50 -0700

    new

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,6 @@
 #include "main.h"
 /**
  * print_line - a function that draws a straight line in the terminal.
- * followed by a new line.
- * @n: An input integer
- * Return: Always 0
  */
 void more_numbers(void)
 {
