f605eff21eb7e970f1255c504c6d64acedb35b1c
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-08 07:21:35 -0700

    new

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,11 +9,11 @@ void print_line(int n)
 {
 	int i = 0;
 
-		if (n > 0)
-		{
-			for (; i < n; i++)
-				_putchar('_');
-		}
+	if (n > 0)
+	{
+		for (; i < n; i++)
+			_putchar('_');
+	}
 		
-		_putchar('\n');
+	_putchar('\n');
 }
