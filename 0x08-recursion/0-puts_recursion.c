72ea21de67dae0712b085a511ead13fc774f3753
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-21 11:52:34 -0700

    recursive

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,14 +7,11 @@
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\n')
-{
-	_putchar('\n');
-	return;
-}
-else
+if (*s)
 {
 	_putchar(*s);
-	_puts_recursion(++s);
-}	
+	_puts_recursion(s + 1);
+}
+else
+	_putchar('\n');
 }
