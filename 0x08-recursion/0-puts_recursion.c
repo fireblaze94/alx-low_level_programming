5382d07e0f255d550442d7f872e38426f3c7495a
fireblaze94 <fireblaze.ij@gmail.com>
2022-07-21 11:49:28 -0700

    recursive

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_puts_recursion - prints a string, followed by new line
+ *@s: character to be printed
+ *Return void
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\n')
+{
+	_putchar('\n');
+	return;
+}
+else
+{
+	_putchar(*s);
+	_puts_recursion(++s);
+}	
+}
